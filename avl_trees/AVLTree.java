89593f6d092e415dcfa29ff876f1a20cfd692f05
Valentin ISHIMWE <ishimwevalentin3@gmail.com>
2023-03-11 12:28:24 +0200

    Show rotations

--- avl_trees/AVLTree.java
+++ avl_trees/AVLTree.java
@@ -4,6 +4,7 @@ public class AVLTree {
 
     private class AVLNode {
         private int value;
+        private int height;
         private AVLNode leftChild;
         private AVLNode rightChild;
 
@@ -15,40 +16,68 @@ public class AVLTree {
     private AVLNode root;
 
     public void insert(int value) {
-        if (root == null) {
-            root = new AVLNode(value);
-            return;
-        }
-
-        insertNode(root, value);
+        root = insertNode(root, value);
     }
 
-    private void insertNode(AVLNode node, int value) {
-        if (node.value > value) {
-            if (node.leftChild == null) {
-                node.leftChild = new AVLNode(value);
-                return;
-            }
-            insertNode(node.leftChild, value);
-            return;
+    private AVLNode insertNode(AVLNode node, int value) {
+        if (node == null) {
+            node = new AVLNode(value);
+            node.height = 0;
+            return node;
         }
 
-        if (node.rightChild == null) {
-            node.rightChild = new AVLNode(value);
-            return;
+        if (node.value > value)
+            node.leftChild = insertNode(node.leftChild, value);
+        else
+            node.rightChild = insertNode(node.rightChild, value);
+
+        var leftHeight = nodeHeight(node.leftChild);
+        var rightHeight = nodeHeight(node.rightChild);
+
+        balance(node);
+        node.height = Math.max(leftHeight, rightHeight) + 1;
+        return node;
+    }
+
+    private void balance(AVLNode node) {
+        if (isLeftHeavy(node)) {
+            if (getBalanceFactor(node.leftChild) < 0)
+                System.out.println("left rotation " + node.leftChild.value);
+            System.out.println("right rotation " + node.value);
         }
-        insertNode(node.rightChild, value);
+        if (isRightHeavy(node)) {
+            if (getBalanceFactor(node.rightChild) > 0)
+                System.out.println("right rotation " + node.rightChild.value);
+            System.out.println("left rotation " + node.value);
+        }
     }
 
     private void printNode(AVLNode node, boolean isLeft, int depth) {
         if (node == null)
             return;
         printNode(node.rightChild, false, depth + 1);
-        System.out.print("|-".repeat(depth));
+        System.out.print("   ".repeat(depth));
+        System.out.print("(" + getBalanceFactor(node) + ") - ");
         System.out.print(node.value);
         System.out.println();
         printNode(node.leftChild, true, depth + 1);
 
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return getBalanceFactor(root) > 1;
+    }
+
+    public boolean isRightHeavy(AVLNode node) {
+        return getBalanceFactor(node) < -1;
+    }
+
+    private int getBalanceFactor(AVLNode node) {
+        return node == null ? 0 : nodeHeight(node.leftChild) - nodeHeight(node.rightChild);
+    }
+
+    private int nodeHeight(AVLNode node) {
+        return node == null ? -1 : node.height;
     }
 
     public void print() {
--- avl_trees/Main.java
+++ avl_trees/Main.java
@@ -4,11 +4,17 @@ public class Main {
     public static void main(String[] args) {
         var avl_tree = new AVLTree();
 
-        avl_tree.insert(3);
+        avl_tree.insert(4);
         avl_tree.insert(9);
         avl_tree.insert(13);
         avl_tree.insert(0);
+        avl_tree.insert(1);
         avl_tree.insert(2);
+        avl_tree.insert(-1);
+        avl_tree.insert(-3);
+        avl_tree.insert(-4);
+        avl_tree.insert(-5);
+        avl_tree.insert(10);
 
         avl_tree.print();
     }
